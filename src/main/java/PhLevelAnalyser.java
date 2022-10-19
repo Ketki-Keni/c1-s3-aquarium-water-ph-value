6d956644b726c6f7b758be3d9f2aa9924d1d531b
Ketki Keni <sketki220@gmail.com>
2022-10-19 16:20:14 +0530

    written logic to check the condition of the ph value of water

--- src/main/java/PhLevelAnalyser.java
+++ src/main/java/PhLevelAnalyser.java
@@ -3,8 +3,17 @@ import java.util.Scanner;
 public class PhLevelAnalyser {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the ph value of the aquarium: ");
         int phValue = scanner.nextInt();
-        System.out.print("Enter the ph value of the aquarium: " + phValue );
+        if (phValue < 7){
+            System.out.println("ph value is low, partial water change required");
+        }
+        else if (phValue >= 7 && phValue <=8){
+            System.out.println("ph value is fine");
+        }
+        else if (phValue > 8){
+            System.out.println("ph value is high, partial water change required");
+        }
 
         scanner.close();
     }
