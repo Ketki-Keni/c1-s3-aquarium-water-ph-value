c4b786c5e3e9777b6d001cdab80cff853f9dbca7
Ketki Keni <sketki220@gmail.com>
2022-10-19 16:10:48 +0530

    utilized scanner functionality to read value from the user

--- src/main/java/PhLevelAnalyser.java
+++ src/main/java/PhLevelAnalyser.java
@@ -3,5 +3,9 @@ import java.util.Scanner;
 public class PhLevelAnalyser {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int phValue = scanner.nextInt();
+        System.out.print("Enter the ph value of the aquarium: " + phValue );
+
+        scanner.close();
     }
 }
